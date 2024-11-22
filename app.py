f47fe4e27710b02aa49c64da4548509b8125b3b6
Austin Labador <labadora@classwork.engr.oregonstate.edu>
2024-11-21 17:00:16 -0800

    Implement basic read and create for Customers table

--- app.py
+++ app.py
@@ -5,12 +5,6 @@
 
 # app = Flask(__name__)
 
-# app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-# app.config['MYSQL_USER'] = 'cs340_labadora'
-# app.config['MYSQL_PASSWORD'] = '7708' #last 4 of onid
-# app.config['MYSQL_DB'] = 'cs340_labadora'
-# app.config['MYSQL_CURSORCLASS'] = "DictCursor"
-
 
 # mysql = MySQL(app)
 
@@ -39,18 +33,18 @@
 #     #Start the app on port 3000, it will be different once hosted
 #     app.run(port=33002, debug=True)
 
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
 import os
 import database.db_connector as db
-# import database.db_credentials as db_creds
+
 
 # Configuration
 
 app = Flask(__name__)
-# db_connection = db.connect_to_database(db_creds.host, db_creds.user, db_creds.passwd, db_creds.db)
 db_connection = db.connect_to_database()
-
-# Routes 
+#comes from module, is essentially MySQL(app) from starter code
 
 # Routes 
 
@@ -62,27 +56,39 @@ def root():
 def bsg_people():
     return "This is the bsg-people route."
 
-@app.route('/customers')
+@app.route('/customers', methods = ["POST", "GET"])
 def customers():
     # Write the query and save it to a variable
-    query = "SELECT * FROM Customers"
+    if request.method == "GET":
+        query = "SELECT * FROM Customers"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
 
-    # The way the interface between MySQL and Flask works is by using an
-    # object called a cursor. Think of it as the object that acts as the
-    # person typing commands directly into the MySQL command line and
-    # reading them back to you when it gets results
-    cursor = db.execute_query(db_connection=db_connection, query=query)
+        # The cursor.fetchall() function tells the cursor object to return all
+        # the results from the previously executed
+        results = cursor.fetchall()
+        # Sends the results back to the web browser.
+        return render_template("customers.j2", customers = results)
 
-    # The cursor.fetchall() function tells the cursor object to return all
-    # the results from the previously executed
-    #
-    # The json.dumps() function simply converts the dictionary that was
-    # returned by the fetchall() call to JSON so we can display it on the
-    # page.
-    results = json.dumps(cursor.fetchall())
+    if request.method == "POST":
+        # runs if user presses add button
+        if request.form.get("Add_Customer"):
+            # grabs user form inputs
+            name = request.form["name"]
+            email = request.form["email"]
+            phone = request.form["phone"]
 
-    # Sends the results back to the web browser.
-    return results
+            # no null inputs
+            query = "INSERT INTO Customers (name, email, phone) VALUES (%s, %s, %s)"
+            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(name,email,phone))
+            results = cursor.fetchall()
+
+            # cursor.execute(query, (name, email, phone))
+            # db.connection.commit()
+
+        # redirect back to people page
+        return redirect("/customers")
+
+
 
 # Listener
 
